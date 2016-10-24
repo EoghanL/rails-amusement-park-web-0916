aa28f14d718d607a3e428d71d34a58262e4edc17
github username <github email address>
2016-10-24 18:20:33 -0400

    models still look good

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :nausea, :happiness, :height, :tickets, :password, :admin)
+  end
+end

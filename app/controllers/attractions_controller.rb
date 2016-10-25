dc6842c925e40e14e5adb36564f361af9da5bf00
github username <github email address>
2016-10-25 13:03:45 -0400

    we do it

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -2,12 +2,45 @@ class AttractionsController < ApplicationController
 
   def index
     @attractions = Attraction.all
+    @user = User.find(session[:user_id])
   end
 
   def show
     @attraction = Attraction.find(params[:id])
     @user = User.find(session[:user_id])
     session[:attraction_id] = @attraction.id
+  end
+
+  def new
+    if current_user.admin
+      @attraction = Attraction.new
+    else
+      redirect_to attractions_path
+    end
+
+  end
+
+  def create
+    @attraction = Attraction.create(attraction_params)
+    redirect_to attraction_path(@attraction)
+  end
+
+  def edit
+    @attraction = Attraction.find(params[:id])
+
+  end
+
+  def update
+    @attraction = Attraction.find(params[:id])
+    @attraction.update(attraction_params)
+    redirect_to attraction_path(@attraction)
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :nausea_rating, :happiness_rating,
+    :min_height, :tickets)
   end
 
 end
